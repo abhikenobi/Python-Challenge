4149ec79009c2805c81f78c978ebbd8e8d655cf8
Abhinav Shrestha <79868048+abhikenobi@users.noreply.github.com>
2021-03-25 01:07:47 -0600

    Updating python code, bank.py is primary file for now

--- Py-Bank/bank.py
+++ Py-Bank/bank.py
@@ -3,17 +3,22 @@ import os
 import csv
 
 #set up variables
-total = 0
-profitloss = []
-changecalc = []
-num_months = 0
-datechange = []
-totalchange = 0
-avg_change = 0
+#the total profit/loss for the whole dataset
+net_total = 0
+#previous month's profit/loss
+prev_profitloss = 0
+#the total number of months/rows in the dataset
+totalmonths = 0
+#average between the change in each month's profit/loss
+avg_monthly_change = 0
+#the greatest positive change in each month's profit/loss
 maxincrease = 0
+#the greatest negative change in each month's profit/loss
 minincrease = 0
-dateincrease = ''
-datedecrease = ''
+#the change in each month's profit/loss
+net_change = 0
+#list to house all the monthly changes in profit/loss
+netchange =[]
 
 #set up file path
 budget_csv = os.path.join("Resources", "budget_data.csv")
@@ -23,20 +28,29 @@ with open(budget_csv) as csvfile:
     #use csvreader to read csv file
     csvreader = csv.reader(csvfile, delimiter = ",")
     
-    #reading the header row
+    #reading the header row to skip during iterations
     csv_header = next(csvreader)
-    #find total months
-    #find the number of lines of data in the file
-    #1 line = 1 month, so total month = length of file minus header
-    totalmonths = len(list(csvreader))
-    
+
+    #have to extract first row of data to keep net monthly change in line with the rest of the data
+    first_row = next(csvreader)
+    #add the same counters as in the for loop
+    totalmonths += 1
+    net_total += int(first_row[1])
+    net_change = nettotal - int(first_row[1])
+    #define previous month's profit/loss, to get ready for the loop
+    prevtotal = int(first_row[1])
+
     for row in csvreader:
-        profitloss.append(int(row[1]))
+        #testing to see if loop works, comment out when code is finalized
+        #print(row)
+        #total months counter
+        totalmonths += 1
+        #net total counter (net total = prev. net total + this month's profit/loss)
+        nettotal += int(row[1])
 
-        print(row)
-    
-
-#fint net total $
+        #finding monthly change
+        #adding each months profit/loss to a list
+        netchange.append(int(row[1]))
 
 #average change
 
@@ -48,5 +62,5 @@ with open(budget_csv) as csvfile:
 print("--------------------------Financial Analysis------------------------")
 print("--------------------------------------------------------------------")
 print(f"Total Months: {totalmonths}")
-print(total)
+print(f"Net Total : ${nettotal}")
 #write into .txt file in Analysis folder
--- Py-Bank/main.py
+++ Py-Bank/main.py
@@ -10,10 +10,42 @@ with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    for row in csvreader:
-        print(row)
-
+    print("Financial Analysis")
+    print("---------------------------------")
     
+    #creating a list for each month in the csv file
+    month = []
+
+    #list for proftloss
+    profitloss = []
+
+    #initial value and variable for net total
+    total = 0
+
+    for row in csvreader:
+        
+        # adding each month to the month list
+        month.append(row[0])
+
+        #adding each month's proft/loss to list
+        profitloss.append(row[1])
+    
+    profitloss.pop(0)
+    
+    for x in profitloss:
+        #add profit/losses from each month to the net total
+        total = sum(profitloss)
+    
+    #finding the total number of months by finding the lenght -1 because of the header
+    #could also do mylist.pop(0) to remove header from list and print out len(month) instead
+    print(f"Total Month : {len(month)-1}")
+
+    print(f" Net Total : {net_total}")
+
+
+
+
+
 
 
 
