43d082dc041e5d98c1aeba91de25b9e910c05866
Abhinav Shrestha <79868048+abhikenobi@users.noreply.github.com>
2021-03-25 18:22:52 -0600

    edit

--- Py-Boss/main.py
+++ Py-Boss/main.py
@@ -5,3 +5,74 @@ import math
 import statistics
 
 #set up variables
+First_Name = []
+Last_Name = []
+DOB = []
+#import state abbreviations from: https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+employee_csv = os.path.join("Resources", "employee_data.csv")
+
+with open(employee_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    
+    csv_header = next(csvreader)
+
+    #need to split name into first and last name and add to corresponding list
+
+#Goals
+#split name column into first name and last name columns
+#DOB (YYYY-MM-DD) rewritten as (MM/DD/YYYY)
+#SSN (###-##-####) rewritten to hide first 5 numbers (***-**-####)
+#abbreviate state
